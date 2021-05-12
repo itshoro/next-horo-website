1ba2ba4af0fd472f154dcd0b105159ec21212b84
Tim Nelke <tim.nelke@outlook.com>
2021-05-12 03:06:04 +0200

    Modified `tailwind.config.js` - custom colors now respect the `bg-opacity` and `text-opacity` classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,33 @@
 module.exports = {
   mode: "jit",
-  purge: [
-    "./pages/**/*.js",
-    "./components/**/*.js"
-  ],
+  purge: ["./pages/**/*.js", "./components/**/*.js"],
   darkMode: "media", // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        foxfire: "hsl(348deg 91% 50%)",
-        ember: "hsl(331deg 88% 46%)"
+        foxfire: ({ opacityVariable, opacityValue }) => {
+          if (opacityValue !== undefined) {
+            return `hsla(348deg,91%,50%,${opacityValue})`;
+          }
+          if (opacityVariable !== undefined) {
+            return `hsla(348deg,91%,50%,${opacityVariable})`;
+          }
+          return `hsl(348deg,91%,50%)`;
+        },
+        ember: ({ opacityVariable, opacityValue }) => {
+          if (opacityValue !== undefined) {
+            return `hsla(331deg,88%,46%,${opacityValue})`;
+          }
+          if (opacityVariable !== undefined) {
+            return `hsla(331deg,88%,46%,${opacityVariable})`;
+          }
+          return `hsl(331deg,88%,46%)`;
+        }
       }
-    },
+    }
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
+  plugins: []
+};
