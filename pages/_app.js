c79dc5f8cb1e8a90cea22ef255b4077f81975c4f
Tim Nelke <tim.nelke@outlook.com>
2021-05-14 05:29:59 +0200

    Moved `Navigation` to its own component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import Navigation from "@/components/nav";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="relative">
+      <Navigation />
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
