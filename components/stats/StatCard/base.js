bb07f0d88a7530858ffbdef0b96b26a6038afbb2
Tim Nelke <tim.nelke@outlook.com>
2021-06-07 22:56:34 +0200

    Modified `StatCard` to reflect CSS color naming changes

--- components/stats/StatCard/base.js
+++ components/stats/StatCard/base.js
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 
 const StatCard = ({ title, children }) => (
-  <div className="border border-offblack rounded-lg text-white py-2 px-4 text-sm">
+  <div className="border border-accent rounded-lg text-white py-2 px-4 text-sm">
     <div className="capitalize pb-2">{title}</div>
     {children}
   </div>
