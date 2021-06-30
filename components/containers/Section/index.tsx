a2740d76e303b0b44b58d6e8bf60f2d6053a92a5
Tim Nelke <tim.nelke@outlook.com>
2021-06-30 21:25:58 +0200

    Moved section to typescript

--- components/containers/Section/index.tsx
+++ components/containers/Section/index.tsx
@@ -1,7 +1,17 @@
+import { CSSProperties, ReactNode } from "react";
+
 const sectionPaddingX = "px-6 sm:px-12";
 const sectionMarginX = "mx-6 sm:mx-12";
 
-const Section = ({ children, className, style }) => (
+const Section = ({
+  children,
+  className,
+  style,
+}: {
+  children?: ReactNode;
+  className?: string;
+  style?: CSSProperties;
+}) => (
   <section
     className={["w-full max-w-[1260px] mx-auto", className, sectionPaddingX]
       .filter((x) => x)
@@ -12,7 +22,15 @@ const Section = ({ children, className, style }) => (
   </section>
 );
 
-const InnerSection = ({ children, className, style }) => (
+const InnerSection = ({
+  children,
+  className,
+  style,
+}: {
+  children?: ReactNode;
+  className?: string;
+  style?: CSSProperties;
+}) => (
   <Section>
     <div
       className={["max-w-[720px] mx-auto", className]
