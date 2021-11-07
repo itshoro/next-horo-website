ebfc73f09bef27485bcb5dad7ec1aae9078d2f98
Tim Nelke <tim.nelke@outlook.com>
2021-11-07 16:40:20 +0100

    Modified design of `ProjectItem`

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,95 +1,60 @@
 import { Text } from "@/components/text";
-import Icon from "@/components/icons";
-import Image from "next/image";
 
 const { Section } = require("@/components/containers/Section");
 
-const ProjectItem = ({
-  title,
-  description,
-  tags,
-  status,
-  img,
-  href,
-}: ProjectItemArgs) => {
+const ProjectItem = ({ title, description, tags, href }: ProjectItemArgs) => {
   return (
-    <article className="w-full">
-      {/* <div className="relative h-96 rounded-lg overflow-hidden">
-        <Image src={img} layout="fill" objectFit="cover" />
-      </div> */}
-      <div className="grid grid-cols-[2fr,4fr] pt-8 pb-16">
-        <div>
-          <Text
-            as="h3"
-            color="foreground"
-            weight="semibold"
-            className="pb-1 mb-1 text-lg flex gap-4"
-          >
-            {title}
-            {status && (
-              <div className="bg-[#ffadbd] dark:bg-[#34141a] inline-block px-3 rounded-lg">
-                <Text
-                  as="div"
-                  color="highlight"
-                  className="inline-block text-xs"
-                >
-                  {status}
-                </Text>
-              </div>
-            )}
-          </Text>
-          <div className="flex gap-4">
-            <div>
-              <Text
-                as="div"
-                color="foreground"
-                className="inline-flex items-center px-4 py-2 gap-2 rounded-lg border border-accent hover:bg-white/10 focus:bg-white/10 active:bg-white/[.15] select-none cursor-pointer transition-colors duration-150 text-sm"
+    <article className="group select-none mb-16">
+      <a href={href}>
+        <div className="max-w-md">
+          <div>
+            <Text
+              as="h3"
+              color="foreground"
+              weight="semibold"
+              className="mb-2 flex items-center gap-2"
+            >
+              {title}
+              <svg
+                className="h-6 select-none font-normal opacity-0 scale-75 -translate-x-2 group-hover:scale-100 group-hover:translate-x-0 group-hover:opacity-100 transition-all duration-200"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 120 120"
               >
-                <Icon type="github" size={16} />
-                Github
-              </Text>
-            </div>
+                <circle cx="50%" cy="50%" r="58" fill="#1a1a1a" />
+                <text
+                  x="50%"
+                  y="50%"
+                  textAnchor="middle"
+                  dominantBaseline="central"
+                  fill="currentColor"
+                  fontSize="64"
+                >
+                  &rarr;
+                </text>
+              </svg>
+            </Text>
           </div>
+          <div>
+            <Text
+              as="p"
+              color="secondary"
+              weight="normal"
+              className="pb-1 mb-1"
+            >
+              {description}
+            </Text>
+          </div>
+          <div></div>
         </div>
-        <div>
-          <Text as="p" color="secondary" weight="normal" className="pb-1 mb-1">
-            {description}
-          </Text>
-        </div>
-        <div></div>
+      </a>
+      <div>
+        <ul className="flex flex-wrap max-w-96 gap-4 text-white font-mono">
+          {tags.map((tag) => {
+            return <li className="py-1 px-2 bg-accent rounded-lg">{tag}</li>;
+          })}
+        </ul>
       </div>
     </article>
-
-    // <article className="inline-block w-full rounded-lg bg-gradient-to-br from-[hsla(348deg,91%,50%,0.15)] to-bg-white/5 shadow-inner">
-    //   <div class="flex items-center px-6 py-4">
-    //     {!logo && (
-    //       <a className="block" href={href}>
-    //         <div className="rounded-lg bg-black/40 p-3">
-    //           <Icon type="git" size={24} />
-    //         </div>
-    //       </a>
-    //     )}
-
-    //     <ul className="flex items-center space-x-2 text-xs select-none ml-2 pl-2 border-l-2 border-black">
-    //       {tags.map((tag) => (
-    //         <li className="px-3 py-1 rounded-full bg-black/40">{tag}</li>
-    //       ))}
-    //     </ul>
-    //   </div>
-    //   <div className="rounded-lg bg-black/40 border border-white/10 p-6">
-    //     <Text
-    //       as="h3"
-    //       color="foreground"
-    //       weight="semibold"
-    //       className="pb-1 mb-1"
-    //     >
-    //       {title}
-    //     </Text>
-    //     <Text as="div" color="secondary" className="mt-1 pt-2">
-    //       {description}
-    //     </Text>
-    //   </div>
-    // </article>
   );
 };
 
@@ -98,20 +63,15 @@ const Projects = () => {
     {
       title: "BASHO",
       description:
-        "A proof of concept distributed python application that interacts with multiple databases and responds to multiple clients, tries to replicate a pax-counting application.",
+        "A python based distributed system, simulating a pax counter. Built for a university asignment.",
       tags: ["python", "sql", "api", "distributed"],
       href: "https://github.com/itshoro/basho",
-      img: "/profile.jpg",
-      // status: "In Progress",
     },
     {
       title: "MTag",
-      description:
-        "Having been always curious about how media players were able to extract data from various media files, I started implementing a library that explores how this is done. While the development of MTag is currently on hiatus, I am still looking to also implement FLAC and WAV support.",
+      description: "Lightweight ID3v2 Tag reader.",
       tags: ["c#", "byte operations", "files"],
       href: "https://github.com/itshoro/mtag",
-      img: "/profile.jpg",
-      status: "Hiatus",
     },
   ];
 
@@ -122,18 +82,6 @@ const Projects = () => {
           Projects
         </Text>
       </Section>
-
-      <div className="bg-accent pl-2 pr-3 py-2 rounded-lg text-white">
-        <div className="flex items-center">
-          <div className="mx-2">
-            <Icon type="git" />
-          </div>
-          <input
-            placeholder="Search..."
-            className="bg-transparent placeholder-shown:placeholder-skeleton_foreground border-none outline-none"
-          ></input>
-        </div>
-      </div>
 
       <Section className="fles items-start pt-8">
         {projects.map((project) => (
@@ -148,8 +96,6 @@ type ProjectItemArgs = {
   title: string;
   description: string;
   tags: string[];
-  status?: string;
-  img: string;
   href: string;
 };
 
