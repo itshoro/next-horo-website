17191b9a3747d6e39744964ba025eb006f15a4f1
Tim Nelke <tim.nelke@outlook.com>
2022-06-27 14:54:08 +0200

    Card title now is regular sized

--- components/stats/StatCard/base.tsx
+++ components/stats/StatCard/base.tsx
@@ -1,7 +1,7 @@
 import { ReactElement } from "react";
 
 const StatCard = ({ title, children }: StatCardArgs) => (
-  <div className="bg-accent rounded-lg text-secondary py-4 px-6 text-sm">
+  <div className="bg-accent rounded-lg text-secondary py-4 px-6">
     <div className="flex items-baseline justify-between gap-4">
       <div className="capitalize pb-2 flex-shrink-0">{title}</div>
       {children}
